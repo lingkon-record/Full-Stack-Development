d04649cb771ab611c0e00c626836a610a6a46928
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-03-05 10:50:11 +0600

    Create 03-rest-parameter-01.js

---
+++ 01-FrontEnd-Development/04-ES6/03-rest-parameter-01.js
@@ -0,0 +1,7 @@
+// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array
+
+function rest(...Name) {
+     console.log(Name)
+}
+
+rest("Alo", "Jonaki", "Pakhi") 
