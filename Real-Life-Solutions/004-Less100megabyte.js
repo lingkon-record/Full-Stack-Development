f4863007d1304976059a6c725fe03b197e63a2ee
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-04-26 14:34:01 +0600

    Update 004-Less100megabyte.js

--- Real-Life-Solutions/004-Less100megabyte.js
+++ Real-Life-Solutions/004-Less100megabyte.js
@@ -1,6 +1,6 @@
-
-const internetPackage = 100; // in megabytes
-const usedData = 80; // in megabytes
+// script
+const internetPackage = 100; 
+const usedData = 80; 
 
 if (usedData < internetPackage) {
   console.log("You have used less than 100 megabytes of your internet package.");
